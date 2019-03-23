e58a375c267885cc0757bb083b8d6e82ddfe10db
Bathur Lu <44486218+Bathur@users.noreply.github.com>
2019-03-23 14:41:05 +0800

    Update AssetCollector.cs

--- Assets/Cleaner/Editor/AssetCollector.cs
+++ Assets/Cleaner/Editor/AssetCollector.cs
@@ -1,211 +1,181 @@
 /**
-	asset cleaner
-	Copyright (c) 2015 Tatsuhiko Yamamura
-
-    This software is released under the MIT License.
-    http://opensource.org/licenses/mit-license.php
+*asset cleaner
+*Copyright (c) 2015 Tatsuhiko Yamamura
+*
+*This software is released under the MIT License.
+*http://opensource.org/licenses/mit-license.php
 */
-using UnityEngine;
-using System.Collections;
+
+//============================================
+//
+//Modified By Bathur Lu
+//
+//Date:     2019.3.23
+//Website:  http://bathur.cn/
+//
+//============================================
+
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
-using System.Text;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Xml.Serialization;
+using UnityEditor;
+using UnityEngine;
 
 namespace AssetClean
 {
-	public class AssetCollector
-	{
-		public static readonly string exportXMLPath = "referencemap.xml";
+    public class AssetCollector
+    {
+        public List<string> deleteFileList = new List<string>();
+        ClassReferenceCollection classCollection = new ClassReferenceCollection();
+        ShaderReferenceCollection shaderCollection = new ShaderReferenceCollection();
 
-		public List<string> deleteFileList = new List<string> ();
-		List<CollectionData> referenceCollection = new List<CollectionData> ();
+        public bool useCodeStrip = true;
+        public bool saveEditorExtensions = true;
 
-		public bool useCodeStrip = true;
-		public bool saveEditorExtensions = true;
+        public void Collection()
+        {
+            try
+            {
+                deleteFileList.Clear();
 
-		public void Collection (string[] collectionFolders)
-		{
+                if (useCodeStrip)
+                {
+                    classCollection.Collection();
+                }
+                shaderCollection.Collection();
 
-			try {
-				XmlSerializer serialize = new XmlSerializer (typeof(List<CollectionData>));
-				deleteFileList.Clear ();
-				referenceCollection.Clear ();
-				
-				if (File.Exists (exportXMLPath)) {
-					using (var fileStream = new StreamReader (exportXMLPath)) {
-						referenceCollection = (List<CollectionData>)serialize.Deserialize (fileStream);
-						fileStream.Close ();
-					}
-				}
+                // Find assets
+                var files = Directory.GetFiles("Assets", "*.*", SearchOption.AllDirectories)
+                    .Where(item => Path.GetExtension(item) != ".meta")
+                    .Where(item => Path.GetExtension(item) != ".js")
+                    .Where(item => Path.GetExtension(item) != ".dll")
+                    .Where(item => Regex.IsMatch(item, "[\\/\\\\]Gizmos[\\/\\\\]") == false)
+                    .Where(item => Regex.IsMatch(item, "[\\/\\\\]Plugins[\\/\\\\]Android[\\/\\\\]") == false)
+                    .Where(item => Regex.IsMatch(item, "[\\/\\\\]Plugins[\\/\\\\]iOS[\\/\\\\]") == false)
+                    .Where(item => Regex.IsMatch(item, "[\\/\\\\]Resources[\\/\\\\]") == false);
 
-				List<IReferenceCollection> collectionList = new List<IReferenceCollection> ();
+                if (useCodeStrip == false)
+                {
+                    files = files.Where(item => Path.GetExtension(item) != ".cs");
+                }
 
-				if (useCodeStrip) {
-					collectionList.Add (new ClassReferenceCollection (saveEditorExtensions));
-				}
+                foreach (var path in files)
+                {
+                    var guid = AssetDatabase.AssetPathToGUID(path);
+                    deleteFileList.Add(guid);
+                }
+                EditorUtility.DisplayProgressBar("Checking", "Collection all files", 0.2f);
+                UnregistReferenceFromResources();
 
-				collectionList.AddRange (new IReferenceCollection[] { 
-					new ShaderReferenceCollection (),
-					new AssetReferenceCollection (),
-				});
+                EditorUtility.DisplayProgressBar("Checking", "Check reference from resources", 0.4f);
+                UnregistReferenceFromScenes();
 
-				foreach (var collection in collectionList) {
-					collection.Init (referenceCollection); 
-					collection.CollectionFiles (); 
-				}
+                EditorUtility.DisplayProgressBar("Checking", "Check reference from scenes", 0.6f);
+                if (saveEditorExtensions)
+                {
+                    UnregistEditorCodes();
+                }
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+            }
+        }
+        void UnregistReferenceFromResources()
+        {
+            var resourcesFiles = Directory.GetFiles("Assets", "*.*", SearchOption.AllDirectories)
+                .Where(item => Regex.IsMatch(item, "[\\/\\\\]Resources[\\/\\\\]") == true)
+                    .Where(item => Path.GetExtension(item) != ".meta")
+                    .ToArray();
+            foreach (var path in AssetDatabase.GetDependencies(resourcesFiles))
+            {
+                UnregistFromDelteList(AssetDatabase.AssetPathToGUID(path));
+            }
+        }
 
-				// Find assets
-				var files = StripTargetPathsAll (useCodeStrip, collectionFolders);
+        void UnregistReferenceFromScenes()
+        {
+            // Exclude objects that reference from scenes.
+            var scenes = EditorBuildSettings.scenes
+                .Where(item => item.enabled == true)
+                    .Select(item => item.path)
+                    .ToArray();
+            foreach (var path in AssetDatabase.GetDependencies(scenes))
+            {
+                if (saveEditorExtensions == false)
+                {
+                    Debug.Log(path);
+                }
+                UnregistFromDelteList(AssetDatabase.AssetPathToGUID(path));
+            }
+        }
 
-				foreach (var path in files) {
-					var guid = AssetDatabase.AssetPathToGUID (path);
-					deleteFileList.Add (guid);
-				}
-				EditorUtility.DisplayProgressBar ("checking", "collection all files", 0.2f);
-				UnregistReferenceFromResources ();
-				
-				EditorUtility.DisplayProgressBar ("checking", "check reference from resources", 0.4f);
-				UnregistReferenceFromScenes ();
+        void UnregistEditorCodes()
+        {
+            // Exclude objects that reference from Editor API
+            var editorcodes = Directory.GetFiles("Assets", "*.cs", SearchOption.AllDirectories)
+                .Where(item => Regex.IsMatch(item, "[\\/\\\\]Editor[\\/\\\\]") == true)
+                    .ToArray();
 
-				EditorUtility.DisplayProgressBar ("checking", "check reference from scenes", 0.6f);
-				if (saveEditorExtensions) {
-					UnregistEditorCodes ();
-				}
+            var undeleteClassList = classCollection.codeFileList
+                .Where(codefile => codefile.Value.Any(guid => deleteFileList.Contains(guid)) == false)
+                    .Select(item => item.Key);
 
-				EditorUtility.DisplayProgressBar ("checking", "check reference from ignorelist", 0.8f);
-				UnregistReferenceFromIgnoreList ();
-				UnregistReferenceFromExtensionMethod ();
+            EditorUtility.DisplayProgressBar("Checking", "Check reference from editor codes", 0.8f);
 
-				using (var fileStream = new StreamWriter (exportXMLPath)) {
-					serialize.Serialize (fileStream, referenceCollection);
-					fileStream.Close ();
-				}
+            foreach (var path in editorcodes)
+            {
+                var code = File.ReadAllText(path);
+                code = Regex.Replace(code, "//.*[\\n\\r]", "");
+                code = Regex.Replace(code, "/\\*.*[\\n\\r]\\*/", "");
+                if (Regex.IsMatch(code, "(\\[MenuItem|AssetPostprocessor|EditorWindow)"))
+                {
+                    UnregistFromDelteList(AssetDatabase.AssetPathToGUID(path));
+                    continue;
+                }
 
-			} finally {
-				EditorUtility.ClearProgressBar ();
-			}
-		}
+                foreach (var undeleteClass in undeleteClassList)
+                {
+                    if (Regex.IsMatch(code, string.Format("\\[CustomEditor.*\\(\\s*{0}\\s*\\).*\\]", undeleteClass.Name)))
+                    {
+                        UnregistFromDelteList(path);
+                        continue;
+                    }
+                }
+            }
+        }
 
-		List<string> StripTargetPathsAll (bool isUseCodeStrip, string[] pathes)
-		{
-			var files = pathes.SelectMany (c => Directory.GetFiles (c, "*.*", SearchOption.AllDirectories))
-					.Distinct ()
-					.Where (item => Path.GetExtension (item) != ".meta")
-					.Where (item => Path.GetExtension (item) != ".js")
-					.Where (item => Path.GetExtension (item) != ".dll")
-					.Where (item => Regex.IsMatch (item, "[\\/\\\\]Gizmos[\\/\\\\]") == false)
-					.Where (item => Regex.IsMatch (item, "[\\/\\\\]Plugins[\\/\\\\]Android[\\/\\\\]") == false)
-					.Where (item => Regex.IsMatch (item, "[\\/\\\\]Plugins[\\/\\\\]iOS[\\/\\\\]") == false)
-					.Where (item => Regex.IsMatch (item, "[\\/\\\\]Resources[\\/\\\\]") == false);
+        void UnregistFromDelteList(string guid)
+        {
+            if (deleteFileList.Contains(guid) == false)
+            {
+                return;
+            }
+            deleteFileList.Remove(guid);
 
-			if (isUseCodeStrip == false) {
-				files = files.Where (item => Path.GetExtension (item) != ".cs");
-			}
-			
-			return files.ToList ();
-		}
+            if (classCollection.references.ContainsKey(guid) == true)
+            {
 
-		void UnregistReferenceFromIgnoreList ()
-		{
-			var codePaths = deleteFileList.Where (fileName => Path.GetExtension (fileName) == ".cs");
+                foreach (var type in classCollection.references[guid])
+                {
+                    var codePaths = classCollection.codeFileList[type];
+                    foreach (var codePath in codePaths)
+                    {
+                        UnregistFromDelteList(codePath);
+                    }
+                }
+            }
 
-			foreach (var path in codePaths) {
-				var code = ClassReferenceCollection.StripComment (File.ReadAllText (path));
-				if (Regex.IsMatch (code, "static\\s*(partial)*\\s*class")) {
-					UnregistFromDelteList (AssetDatabase.AssetPathToGUID (path));
-					continue;
-				}
-			}
-		}
-
-		void UnregistReferenceFromExtensionMethod ()
-		{
-			var resourcesFiles = deleteFileList
-				.Where (item => Path.GetExtension (item) != ".meta")
-				.ToArray ();
-			foreach (var path in AssetDatabase.GetDependencies (resourcesFiles)) {
-				UnregistFromDelteList (AssetDatabase.AssetPathToGUID (path));
-			}
-		}
-
-		void UnregistReferenceFromResources ()
-		{
-			var resourcesFiles = deleteFileList
-				.Where (item => Regex.IsMatch (item, "[\\/\\\\]Resources[\\/\\\\]") == true)
-				.Where (item => Path.GetExtension (item) != ".meta")
-				.ToArray ();
-			foreach (var path in AssetDatabase.GetDependencies (resourcesFiles)) {
-
-				UnregistFromDelteList (AssetDatabase.AssetPathToGUID (path));
-			}
-		}
-
-		void UnregistReferenceFromScenes ()
-		{
-			// Exclude objects that reference from scenes.
-			var scenes = EditorBuildSettings.scenes
-				.Where (item => item.enabled == true)
-					.Select (item => item.path)
-					.ToArray ();
-			foreach (var path in AssetDatabase.GetDependencies (scenes)) {
-
-				UnregistFromDelteList (AssetDatabase.AssetPathToGUID (path));
-			} 
-		}
-
-		void UnregistEditorCodes ()
-		{
-			// Exclude objects that reference from Editor API
-			var editorcodes = Directory.GetFiles ("Assets", "*.*", SearchOption.AllDirectories)
-				.Where (fileName => Path.GetExtension (fileName) == ".cs")
-				.Where (item => Regex.IsMatch (item, "[\\/\\\\]Editor[\\/\\\\]") == true)
-				.ToArray ();
-			
-			EditorUtility.DisplayProgressBar ("checking", "check reference from editor codes", 0.8f);
-			
-			foreach (var path in editorcodes) {
-				var code = ClassReferenceCollection.StripComment (File.ReadAllText (path));
-				if (Regex.IsMatch (code, "(\\[MenuItem|AssetPostprocessor|EditorWindow)")) {
-					UnregistFromDelteList (AssetDatabase.AssetPathToGUID (path));
-					continue;
-				}
-			}
-			foreach (var path in editorcodes) {
-				var guid = AssetDatabase.AssetPathToGUID (path);
-
-				if (referenceCollection.Exists (c => c.fileGuid == guid) == false) {
-					continue;
-				}
-
-				var referenceGuids = referenceCollection.First (c => c.fileGuid == guid).referenceGids;
-
-
-
-				if (referenceGuids.Any (c => deleteFileList.Contains (c) == true) == false) {
-					UnregistFromDelteList (AssetDatabase.AssetPathToGUID (path));
-				}
-			}
-		}
-
-		void UnregistFromDelteList (string guid)
-		{
-			if (deleteFileList.Contains (guid) == false) {
-				return;
-			}
-			deleteFileList.Remove (guid);
-
-			if (referenceCollection.Exists (c => c.fileGuid == guid)) {
-				var refInfo = referenceCollection.First (c => c.fileGuid == guid);
-				foreach (var referenceGuid in refInfo.referenceGids) {
-					UnregistFromDelteList (referenceGuid);
-				}
-			}
-
-		}
-	}
+            if (shaderCollection.shaderFileList.ContainsValue(guid))
+            {
+                var shader = shaderCollection.shaderFileList.First(item => item.Value == guid);
+                var shaderAssets = shaderCollection.shaderReferenceList[shader.Key];
+                foreach (var shaderPath in shaderAssets)
+                {
+                    UnregistFromDelteList(shaderPath);
+                }
+            }
+        }
+    }
 }
